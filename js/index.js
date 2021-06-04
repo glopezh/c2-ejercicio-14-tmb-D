d4d1cffc737b053b2cf499a7be95bdd06c0fdb37
aleupons <apahoquei@gmail.com>
2021-06-04 14:03:50 +0200

    Pruebas de conexión con las APIS

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
 
 // Datos para las APIs
 const geocodingApi = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
-const mapboxToken = ""; // Mete aquí el Token de Mapbox
+const mapboxToken =
+  "pk.eyJ1IjoicmFsbGdlIiwiYSI6ImNrcGk4azdjbjBndHcydXBjYXc2MzUweTQifQ.GDFAAG5UrMSO4_xPTFqaeg"; // Mete aquí el Token de Mapbox
 const tmbApi = "https://api.tmb.cat/v1/planner/plan";
-const appId = ""; // Mete aquí el app_id de TMB
-const appKey = ""; // Mete aquí el app_key de TMB
+const appId = "0031bdb3"; // Mete aquí el app_id de TMB
+const appKey = "1eef12f633798f1b3f0427e9c6e39525"; // Mete aquí el app_key de TMB
 mapboxgl.accessToken = mapboxToken;
 
 // LLama a esta función para generar el pequeño mapa que sale en cada paso
@@ -30,3 +31,28 @@ const coordenadas = {
     longitud: 0,
   },
 };
+
+// Pruebas MapBox
+const places = "Barcelona.json";
+fetch(`${geocodingApi}${places}?access_token=${mapboxToken}`)
+  .then((response) => response.json())
+  .then((datos) => {
+    const coordenadas = datos.features[0].geometry.coordinates;
+    console.log(coordenadas);
+    return coordenadas;
+  });
+
+// Pruebas TMB
+const fromPlace = "41.3755204,2.1498870"; // Coordenadas de Barcelona
+const toPlace = "41.3789985,2.1377242";
+// Datos no required
+const date = "06-06-2021";
+const time = "14:00";
+const arriveBy = "false"; // false: date y time tipo salida; true: date y time tipo llegada
+const mode = "WALK";
+/* fetch(
+  `${tmbApi}?app_id=${appId}&app_key=${appKey}&fromPlace=${fromPlace}&toPlace=${toPlace}&date=${date}&time=${time}&arriveBy=${arriveBy}&mode=${mode}`
+); */
+fetch(
+  `${tmbApi}?app_id=${appId}&app_key=${appKey}&fromPlace=${fromPlace}&toPlace=${toPlace}`
+);
