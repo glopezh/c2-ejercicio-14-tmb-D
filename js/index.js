9791d9761cbfce933d974f3da785e2bca5a3736a
aleupons <apahoquei@gmail.com>
2021-06-05 14:35:27 +0200

    Adquirir datos de TMB y alimentar HTML con el DOM

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* global mapboxgl */
 
-import { * as dom } from "./dom.js";
+/* import { * as dom } from "./dom.js"; */
 
 // Datos para las APIs
 const geocodingApi = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
@@ -10,8 +10,8 @@ const tmbApi = "https://api.tmb.cat/v1/planner/plan";
 const appId = "0031bdb3"; // Mete aquí el app_id de TMB
 const appKey = "1eef12f633798f1b3f0427e9c6e39525"; // Mete aquí el app_key de TMB
 mapboxgl.accessToken = mapboxToken;
-const mapa =
-  "https://www.google.com/maps?z=19&t=m&q=loc:COORDENADA1+COORDENADA2";
+const mapa = (longitud, latitud) =>
+  `https://www.google.com/maps?z=19&t=m&q=loc:${longitud}+${latitud}`;
 
 // LLama a esta función para generar el pequeño mapa que sale en cada paso
 // Le tienes que pasar un array con las dos coordenadas y el elemento HTML donde tiene que generar el mapa
@@ -42,7 +42,9 @@ let {
 
 // Recogemos las coordenadas de origen y destino con Mapbox
 const placesOrigen = "Sagrada familia"; // String con la dirección de la que queremos las coordenadas (máx 20 words). Lo recogeremos de HTML
-const placesDestino = "catalunya barcelona";
+const placesDestino = "sants barcelona";
+const listaPasos = document.querySelector(".pasos");
+
 // Datos de origen
 fetch(`${geocodingApi}${placesOrigen}.json?access_token=${mapboxToken}`)
   .then((response) => response.json())
@@ -59,18 +61,59 @@ fetch(`${geocodingApi}${placesOrigen}.json?access_token=${mapboxToken}`)
         latitudDestino = latitud;
         longitudDestino = longitud;
 
-        // Mandamos las coordenadas a la API de TMB
-        const fromPlace = `${latitudOrigen},${longitudOrigen}`; // Coordenadas de AM de Barcelona
-        const toPlace = `${latitudDestino},${longitudDestino}`;
-        fetch(
-          `${tmbApi}?app_id=${appId}&app_key=${appKey}&fromPlace=${fromPlace}&toPlace=${toPlace}`
-        )
-          .then((response) => response.json())
-          .then((datos) => {
-            const itinerarios = datos.plan.itineraries;
-            console.log(itinerarios);
-          });
+        // Cuando hacemos submit al formulario se mandan todos los datos
+        const formulario = document.querySelector(".form-coordenadas");
+        formulario.addEventListener("submit", (e) => {
+          e.preventDefault();
+          // Mandamos las coordenadas a la API de TMB
+          const fromPlace = `${latitudOrigen},${longitudOrigen}`; // Coordenadas de AM de Barcelona
+          const toPlace = `${latitudDestino},${longitudDestino}`;
+          fetch(
+            `${tmbApi}?app_id=${appId}&app_key=${appKey}&fromPlace=${fromPlace}&toPlace=${toPlace}`
+          )
+            .then((response) => response.json())
+            .then((datos) => {
+              const itinerario = datos.plan.itineraries[0];
+              const pasos = itinerario.legs;
+              let i = 1;
+              for (const {
+                from,
+                to,
+                distance,
+                duration,
+                startTime,
+                endTime,
+              } of pasos) {
+                const nuevoPaso = document
+                  .querySelector(".paso.paso-dummy")
+                  .cloneNode(true);
+                nuevoPaso.classList.remove("paso-dummy");
+                const pasoEncabezado =
+                  nuevoPaso.querySelector(".paso-encabezado");
+                const pasoNumero = pasoEncabezado.querySelector(".paso-numero");
+                pasoNumero.textContent = i;
+                const pasoFrom = pasoEncabezado.querySelector(".paso-from");
+                pasoFrom.textContent = from.name;
+                const pasoTo = pasoEncabezado.querySelector(".paso-to");
+                pasoTo.textContent = to.name;
+                const linkMapa = pasoEncabezado.querySelector(".paso-mapa");
+                linkMapa.href = mapa(to.lat, to.lon);
+                const pasoHora = nuevoPaso.querySelector(".paso-hora .dato");
+                pasoHora.textContent = startTime;
+                const pasoDistancia = nuevoPaso.querySelector(
+                  ".paso-distancia .dato"
+                );
+                pasoDistancia.textContent = distance;
+                const pasoDuracion = nuevoPaso.querySelector(
+                  ".paso-duracion .dato"
+                );
+                pasoDistancia.textContent = duration;
+                const mapaPaso = nuevoPaso.querySelector(".mapa");
+                generaMapa([to.lat, to.lon], mapaPaso);
+                listaPasos.append(nuevoPaso);
+                i++;
+              }
+            });
+        });
       });
   });
-
-// Integración API Mapbox (obtenemos coordenadas) + API TMB (pasos a seguir para la ruta)
