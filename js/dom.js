f886d67835d30a37040dcc789526ca7b3726886f
federico <federicotiralongo95@gmail.com>
2021-06-06 13:37:34 +0200

    creacion de la funcion que devuelve la ubicacion propria

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,53 @@
-// export
+// Al pulsar introducir dirección tiene que aparecer el input
+const formulario = document.querySelector(".form-coordenadas");
 
-// Al pulsar introducir dirección tiene que aparecer el input y recoger el string con JS
+const inputDireccion = (nodo, boton, input, boolean) => {
+  nodo.querySelector(boton).addEventListener("click", () => {
+    const clase = nodo.querySelector(`${input}.direccion-definitiva`).classList;
+    if (boolean) {
+      clase.add("on");
+    } else {
+      clase.remove("on");
+    }
+  });
+};
+inputDireccion(formulario, "#de-direccion", ".de-direccion-definitiva", true);
+inputDireccion(formulario, "#a-direccion", ".a-direccion-definitiva", true);
+inputDireccion(
+  formulario,
+  "#de-mi-ubicacion",
+  ".de-direccion-definitiva",
+  false
+);
+inputDireccion(formulario, "#a-mi-ubicacion", ".a-direccion-definitiva", false);
+
+// Recogemos string del input
+const direccionOrigen = formulario.querySelector(
+  ".de-direccion-definitiva.direccion-definitiva"
+);
+const direccionDestino = formulario.querySelector(
+  ".a-direccion-definitiva.direccion-definitiva"
+);
+const direcciones = (nodo) => {
+  nodo.addEventListener("keydown", (e) => {
+    setTimeout(() => {
+      if (nodo.value !== "") {
+        const direccion = e.target.value;
+        console.log(direccion);
+        return direccion; // Este es el string que tenemos que exportar
+      }
+    }, 500);
+  });
+};
+
+export const direccionDesde = direcciones(direccionOrigen); // No funciona. No puedo exportar datos dentro del evento
+export const direccionHasta = direcciones(direccionDestino);
+console.log(direccionDesde); // Undefined porque se ejecuta al principio y ya está
 
 // Al pulsar usar mi ubicación tenemos que obtener la localización del navegador en un string de coordenadas
+const miUbicacion = (latitud, longitud) => {
+  navigator.geolocation.getCurrentPosition((pos) => {
+    latitud = pos.coords.latitude;
+    longitud = pos.coords.longitude;
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* global mapboxgl */
 
-import { * as dom } from "./dom.js";
+import { direccionDesde, direccionHasta } from "./dom.js";
 
 // Datos para las APIs
 const geocodingApi = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
@@ -10,8 +10,8 @@ const tmbApi = "https://api.tmb.cat/v1/planner/plan";
 const appId = "0031bdb3"; // Mete aquí el app_id de TMB
 const appKey = "1eef12f633798f1b3f0427e9c6e39525"; // Mete aquí el app_key de TMB
 mapboxgl.accessToken = mapboxToken;
-const mapa =
-  "https://www.google.com/maps?z=19&t=m&q=loc:COORDENADA1+COORDENADA2";
+const mapa = (longitud, latitud) =>
+  `https://www.google.com/maps?z=19&t=m&q=loc:${longitud}+${latitud}`;
 
 // LLama a esta función para generar el pequeño mapa que sale en cada paso
 // Le tienes que pasar un array con las dos coordenadas y el elemento HTML donde tiene que generar el mapa
@@ -24,53 +24,102 @@ const generaMapa = (coordenadas, mapa) => {
   });
 };
 
-// Coordenadas que se mandarán a la API de TMB. Tienes que alimentar este objeto a partir de las coordenadas que te dé la API de Mapbox
-const coordenadas = {
-  desde: {
-    latitud: 0,
-    longitud: 0,
-  },
-  hasta: {
-    latitud: 0,
-    longitud: 0,
-  },
-};
-let {
-  desde: { latitud: latitudOrigen, longitud: longitudOrigen },
-  hasta: { latitud: latitudDestino, longitud: longitudDestino },
-} = coordenadas;
+// Cuando hacemos submit al formulario se mandan todos los datos
+const formulario = document.querySelector(".form-coordenadas");
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-// Recogemos las coordenadas de origen y destino con Mapbox
-const placesOrigen = "Sagrada familia"; // String con la dirección de la que queremos las coordenadas (máx 20 words). Lo recogeremos de HTML
-const placesDestino = "catalunya barcelona";
-// Datos de origen
-fetch(`${geocodingApi}${placesOrigen}.json?access_token=${mapboxToken}`)
-  .then((response) => response.json())
-  .then((datos) => {
-    const [longitud, latitud] = datos.features[0].geometry.coordinates; // Array de coordenadas [latitud,longitud]
-    latitudOrigen = latitud;
-    longitudOrigen = longitud;
+  // Coordenadas que se mandarán a la API de TMB. Tienes que alimentar este objeto a partir de las coordenadas que te dé la API de Mapbox
+  const coordenadas = {
+    desde: {
+      latitud: 0,
+      longitud: 0,
+    },
+    hasta: {
+      latitud: 0,
+      longitud: 0,
+    },
+  };
+  let {
+    desde: { latitud: latitudOrigen, longitud: longitudOrigen },
+    hasta: { latitud: latitudDestino, longitud: longitudDestino },
+  } = coordenadas;
 
-    // Datos de destino
-    fetch(`${geocodingApi}${placesDestino}.json?access_token=${mapboxToken}`)
-      .then((response) => response.json())
-      .then((datos) => {
-        const [longitud, latitud] = datos.features[0].geometry.coordinates; // Array de coordenadas [latitud,longitud]
-        latitudDestino = latitud;
-        longitudDestino = longitud;
+  // Recogemos las coordenadas de origen y destino con Mapbox
+  const placesOrigen = formulario.querySelector(
+    ".de-direccion-definitiva.direccion-definitiva"
+  ).value; // String con la dirección de la que queremos las coordenadas (máx 20 words). Lo recogeremos de HTML
+  const placesDestino = formulario.querySelector(
+    ".a-direccion-definitiva.direccion-definitiva"
+  ).value;
 
-        // Mandamos las coordenadas a la API de TMB
-        const fromPlace = `${latitudOrigen},${longitudOrigen}`; // Coordenadas de AM de Barcelona
-        const toPlace = `${latitudDestino},${longitudDestino}`;
-        fetch(
-          `${tmbApi}?app_id=${appId}&app_key=${appKey}&fromPlace=${fromPlace}&toPlace=${toPlace}`
-        )
-          .then((response) => response.json())
-          .then((datos) => {
-            const itinerarios = datos.plan.itineraries;
-            console.log(itinerarios);
-          });
-      });
-  });
+  // Datos de origen
+  fetch(`${geocodingApi}${placesOrigen}.json?access_token=${mapboxToken}`)
+    .then((response) => response.json())
+    .then((datos) => {
+      const [longitud, latitud] = datos.features[0].geometry.coordinates; // Array de coordenadas [latitud,longitud]
+      latitudOrigen = latitud;
+      longitudOrigen = longitud;
 
-// Integración API Mapbox (obtenemos coordenadas) + API TMB (pasos a seguir para la ruta)
+      // Datos de destino
+      fetch(`${geocodingApi}${placesDestino}.json?access_token=${mapboxToken}`)
+        .then((response) => response.json())
+        .then((datos) => {
+          const [longitud, latitud] = datos.features[0].geometry.coordinates; // Array de coordenadas [latitud,longitud]
+          latitudDestino = latitud;
+          longitudDestino = longitud;
+
+          // Mandamos las coordenadas a la API de TMB
+          const fromPlace = `${latitudOrigen},${longitudOrigen}`; // Coordenadas de AM de Barcelona
+          const toPlace = `${latitudDestino},${longitudDestino}`;
+          fetch(
+            `${tmbApi}?app_id=${appId}&app_key=${appKey}&fromPlace=${fromPlace}&toPlace=${toPlace}`
+          )
+            .then((response) => response.json())
+            .then((datos) => {
+              // Alimentamos el HTML con los datos de la API de TMB
+              const listaPasos = document.querySelector(".pasos");
+              const itinerario = datos.plan.itineraries[0];
+              const pasos = itinerario.legs;
+              let i = 1;
+              for (const {
+                from,
+                to,
+                distance,
+                duration,
+                startTime,
+                endTime,
+              } of pasos) {
+                const nuevoPaso = document
+                  .querySelector(".paso.paso-dummy")
+                  .cloneNode(true);
+                nuevoPaso.classList.remove("paso-dummy");
+                const pasoEncabezado =
+                  nuevoPaso.querySelector(".paso-encabezado");
+                const pasoNumero = pasoEncabezado.querySelector(".paso-numero");
+                pasoNumero.textContent = i;
+                const pasoFrom = pasoEncabezado.querySelector(".paso-from");
+                pasoFrom.textContent = from.name;
+                const pasoTo = pasoEncabezado.querySelector(".paso-to");
+                pasoTo.textContent = to.name;
+                const linkMapa = pasoEncabezado.querySelector(".paso-mapa");
+                linkMapa.href = mapa(to.lat, to.lon);
+                const pasoHora = nuevoPaso.querySelector(".paso-hora .dato");
+                pasoHora.textContent = startTime;
+                const pasoDistancia = nuevoPaso.querySelector(
+                  ".paso-distancia .dato"
+                );
+                pasoDistancia.textContent = distance;
+                const pasoDuracion = nuevoPaso.querySelector(
+                  ".paso-duracion .dato"
+                );
+                pasoDistancia.textContent = duration;
+                const mapaPaso = nuevoPaso.querySelector(".mapa");
+                generaMapa([to.lat, to.lon], mapaPaso);
+                listaPasos.append(nuevoPaso);
+                i++;
+              }
+            });
+        });
+    });
+});
